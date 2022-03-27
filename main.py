51764a78474995f57a6fb83fd6a827ecaa80ab0e
sleepntsheep <sleepntsheep@gmail.com>
2022-03-27 21:26:24 +0700

    e

--- main.py
+++ main.py
@@ -33,12 +33,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    replytext: str = 'default'
-    command: str = event.message.text.split()[0]
-    params: List[str] = event.message.text.split()[1:]
-    if (command == 'e'):
-        replytext: str = eval(' '.join(params))
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=replytext))
+    try:
+        replytext: str = 'default'
+        command: str = event.message.text.split()[0]
+        params: List[str] = event.message.text.split()[1:]
+        if (command == 'e'):
+            replytext: str = eval(' '.join(params))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=replytext))
+    except Exception as error:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=error))
 
 if __name__ == '__main__':
     app.run(int(getenv('PORT')))
