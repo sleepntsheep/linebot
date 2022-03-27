7f669697cb555c0e0502ccd9e44643f7ad161721
sleepntsheep <sleepntsheep@gmail.com>
2022-03-27 20:18:21 +0700

    e

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 if __name__ == '__main__':
-    app.run(getenv('PORT') || 8080)
+    app.run(getenv('PORT'))
